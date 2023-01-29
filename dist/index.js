a01074181ae4dcc9f05c7e559e0c8bef5a0485bf
Argo Zhang <argo@live.ca>
2023-01-29 05:21:02 +0000

    chore: update package

--- dist/index.js
+++ dist/index.js
@@ -9820,12 +9820,15 @@ async function run() {
 
     core.info(`Add labels: ${labels} to ${owner}/${repo}#${issueNumber}`);
 
-    await octokit.issues.addLabels({
-      owner,
-      repo,
+    await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
+      owner: owner,
+      repo: repo,
       issue_number: issueNumber,
-      labels,
-    });
+      labels: [
+        'bug',
+        'enhancement'
+      ]
+    })
     
   } catch (error) {
     core.setFailed(error.message);
